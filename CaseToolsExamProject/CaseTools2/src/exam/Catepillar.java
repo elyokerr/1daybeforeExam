411f3f5320e69d48a0ef42fa501ea63e47beb769
samuel demelo <samueldemelo0832@gmail.com>
2024-02-01 10:26:24 +0530

    tyhrhr

---
+++ CaseToolsExamProject/CaseTools2/src/exam/Catepillar.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Catepillar {
+	int moves; 
+	int cabagges;
+	
+	 Catepillar(int moves,int cabagges)
+	 {
+		 this.moves=moves;
+		 this.cabagges=cabagges;
+	
+	 }
+
+}
